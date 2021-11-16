d062a13a2e5671f6a7da6222fba7b52cf6066ce2
francesca <fradeluca85@gmail.com>
2021-11-16 15:57:20 +0100

    pari e dispari

--- dispari/script.js
+++ dispari/script.js
@@ -1,2 +1,17 @@
 // alert('ciao')
 
+
+// Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+//scelta giocatore
+const choose = prompt("Pari o dispari?"); 
+console.log("Hai scelto " + choose);
+
+// numero del giocatore
+const userNumber = parseInt(prompt("Inserisci un numero tra 1 e 5: "));
+if (userNumber > 5){
+    alert ("non hai inserito un numero tra 1 e 5")
+    
+}
+console.log("Il tuo numero è " + userNumber);
+
--- palindroma/script.js
+++ palindroma/script.js
@@ -1,2 +1,3 @@
 // alert('ciao')
 
+// Palidroma Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
